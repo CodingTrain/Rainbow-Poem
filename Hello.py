3a7d841f1ef406e9a24c34a162412fbf00ae5346
Shiv Patel <76883453+shh1v@users.noreply.github.com>
2021-01-19 06:34:48 +0530

    Creat Hello.py
    created a function print_hi().

---
+++ Hello.py
@@ -0,0 +1,10 @@
+def print_hi(name):
+    """
+    return a string of greetings!
+    :param name: String
+    :return: String
+    """
+    print("Hello", name+".", "Hope you are having a great day!")
+
+if __name__ == '__main__':
+    print(print_hi('Shiv Patel'))
